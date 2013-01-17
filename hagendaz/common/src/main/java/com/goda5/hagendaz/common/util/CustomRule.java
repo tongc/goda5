0b194f168b0a0e25116acc7ee89a9963c767cfd1
chentong <chentongtc@gmail.com>
2013-01-17 20:32:41 +0000

    some changes

--- hagendaz/common/src/main/java/com/goda5/hagendaz/common/util/CustomRule.java
+++ hagendaz/common/src/main/java/com/goda5/hagendaz/common/util/CustomRule.java
@@ -6,9 +6,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 import net.sourceforge.pmd.AbstractJavaRule;
 import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.ast.ASTArguments;
+import net.sourceforge.pmd.ast.ASTBlock;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.ASTMethodDeclarator;
 import net.sourceforge.pmd.ast.ASTName;
 import net.sourceforge.pmd.ast.ASTIfStatement;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.ast.ASTResultType;
+import net.sourceforge.pmd.ast.ASTReturnStatement;
+import net.sourceforge.pmd.ast.ASTStatement;
+import net.sourceforge.pmd.ast.ASTStatementExpression;
+import net.sourceforge.pmd.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.SimpleJavaTypeNode;
 
 public class CustomRule extends AbstractJavaRule {
 	/*
@@ -17,6 +29,42 @@ public class CustomRule extends AbstractJavaRule {
 	 */
 	@Override
 	public Object visit(ASTMethodDeclaration node, Object data) {
+		printAllChildrenTypes(node);
+		
+		List<ASTVariableDeclarator> children = node.findChildrenOfType(ASTVariableDeclarator.class);
+		
+		List<ASTName> aaa = node.findChildrenOfType(ASTName.class);
+		for(ASTName a:aaa) {
+			System.out.println("~~~~~~~~~~" + a.getImage());
+		}
+		
+		List<ASTVariableDeclaratorId> bbb = node.findChildrenOfType(ASTVariableDeclaratorId.class);
+		for(ASTVariableDeclaratorId b:bbb) {
+			System.out.println("~~~~~~" + b.getImage());
+		}
+		
+		System.out.println(children.size());
+		for(ASTVariableDeclarator child:children) {
+			System.out.println(child.getLabel() + " " + child.getImage() + " " + child.getClass() + child.getType());
+			System.out.println("tttttt" + child.jjtGetChild(0).getClass());
+			System.out.println("tttttt" + child.jjtGetChild(1).getClass());
+		}
+		
+		List<ASTArguments> children1 = node.findChildrenOfType(ASTArguments.class);
+		System.out.println(children1.size());
+		for(ASTArguments child:children1) {
+			System.out.println(child.getLabel() + " " + child.getImage() + " " + child.getClass() + child.getArgumentCount());
+		}
+		
+		List<ASTStatement> ccccc = node.findChildrenOfType(ASTStatement.class);
+		for(ASTStatement child:ccccc) {
+			System.out.println("#####" + child.jjtGetNumChildren());
+			System.out.println(((ASTStatementExpression)child.jjtGetChild(0)).jjtGetNumChildren());
+			System.out.println("#####" + ((ASTStatementExpression)child.jjtGetChild(0)).jjtGetChild(0).getClass());
+			System.out.println("#####" + ((ASTStatementExpression)child.jjtGetChild(0)).jjtGetChild(0).jjtGetChild(0).getClass());
+		}
+		
+		/*
 		System.out.println(data);
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@" + node.getResultType());
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@" + node.getMethodName());
@@ -28,37 +76,36 @@ public class CustomRule extends AbstractJavaRule {
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@555" + node.isPublic());
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@555" + node.isAbstract());
 		
-		List<ASTIfStatement> isl = node.findChildrenOfType(ASTIfStatement.class);
-		System.out.println("22222222222" + isl.size());
+		List<ASTReturnStatement> isl = node.findChildrenOfType(ASTReturnStatement.class);
+		System.out.println("return statement count " + isl.size());
 		
 		List<ModelAndView> mvs = node.findChildrenOfType(ModelAndView.class);
 		
-		System.out.println("22222222222" + mvs.size());
+		System.out.println("Num of children " + node.jjtGetNumChildren());
 		
-		node.findChildrenOfType(ASTIfStatement.class);
+		System.out.println("11111111aaaaaaa§" + node.jjtGetChild(0).getClass());
+		System.out.println("11111111aaaaaaa§" + ((ASTResultType) node.jjtGetChild(0)).getImage());
+		System.out.println("11111111aaaaaaa§" + node.jjtGetChild(1).getClass());
+		System.out.println("11111111aaaaaaa§" + ((ASTMethodDeclarator) node.jjtGetChild(1)).getImage());
+		System.out.println("11111111aaaaaaa§" + node.jjtGetChild(2).getClass());
+		System.out.println("11111111aaaaaaa§" + ((ASTBlock) node.jjtGetChild(2)).getImage());
 		
-		System.out.println("bbbbbbbb" + node.jjtGetNumChildren());
-		
-		System.out.println("ccccccc" + node.findChildrenOfType(Object.class));
-		
-		System.out.println("aaaaaaaa" + ((ASTName) node.jjtGetChild(0)).getImage());
-		
-		ASTIfStatement is = isl.get(0);
-		System.out.println("11111111111class type " + is.getClass());
-		
-		if (node.getResultType().isVoid()) {
-			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@11111" + node.getResultType());
-			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@33333" + node.getImage());
-			return super.visit(node, data);
-		} else {
-			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@22222" + node.getResultType());
-		}
+		System.out.println("iiiiiiiiiii" + ((ASTBlock) node.jjtGetChild(2)).jjtGetChild(0).getClass());
+		System.out.println("iiiiiiiiiii" + ((ASTBlock) node.jjtGetChild(2)).jjtGetChild(1).getClass());
+		*/
 		return node;
+	}
+	
+	public void printAllChildrenTypes(net.sourceforge.pmd.ast.Node node) {
+		for(int i=0;i<node.jjtGetNumChildren();i++) {
+			System.out.println(node.jjtGetChild(i).getClass());
+			printAllChildrenTypes(node.jjtGetChild(i));
+		}
 	}
 	
 	public static void main(String[] args) {
 		PMD pmd = new PMD();
-		String[] params = new String[]{"/work/git/goda5/hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/", "xml", "/work/git/goda5/hagendaz/common/src/main/resources/pmd.xml"};
+		String[] params = new String[]{"/Users/tonchen/git/goda5/hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/", "xml", "/Users/tonchen/git/goda5/hagendaz/common/src/main/resources/pmd.xml"};
 		pmd.main(params);
 	}
 }
--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/PmdTest.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/PmdTest.java
@@ -3,10 +3,14 @@ package com.goda5.hagendaz.common.util;
 import org.springframework.web.servlet.ModelAndView;
 
 public class PmdTest {
-	public void testMethod() {
+	public ModelAndView testMethod(ModelAndView mv1) {
 		System.out.println("PMD Test Method");
 		
 		ModelAndView mv = new ModelAndView();
+		mv.addObject("dfsdfsdfs");
+		mv.addObject("aaaa", "abbb");
+		
+		return new ModelAndView();
 	}
 	
 	public int testMethod2() {
