b9001bf1fa68b02a69007a9d43aa911ff7be37b3
chentong <chentongtc@gmail.com>
2013-09-23 20:45:19 +0100

    some tests

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/CommonTest.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/util/CommonTest.java
@@ -4,11 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CommonTest {
-	@Test
+	@Test(expectedExceptions=NullPointerException.class)
 	public void testString() {
 		Assert.assertEquals("AAA", "AAA");
 		Assert.assertTrue("AAA" == "AAA");
 		Assert.assertFalse(new String("AAA") == new String("AAA"));
 		Assert.assertTrue("AAA".equals("AAA"));
+		Integer a = null;
+		new CommonTest().test(a);
+	}
+
+	public void test(int a) {
 	}
 }
