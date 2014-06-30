df0e2003bb958c193d26dafd7a47003996136e45
chentongtc@gmail.com <chentongtc@gmail.com>
2014-06-30 23:18:37 +0100

    updated

--- hagendaz/service/src/test/java/com/goda5/hagendaz/service/ZeroMQTest.java
+++ hagendaz/service/src/test/java/com/goda5/hagendaz/service/ZeroMQTest.java
@@ -11,8 +11,8 @@ public class ZeroMQTest {
 	@Test
 	public void testServer() {
 		ActorSystem system = ActorSystem.create("MySystem");
-		ActorRef myActor = system.actorOf(new Props(AkkaActor.class), "myActor");
-		myActor.tell("test");
+		ActorRef myActor = system.actorOf(new Props(null, AkkaActor.class, null), "myActor");
+		myActor.tell("test", myActor);
 		//ActorRef pubSocket = ZeroMQExtension.get(system).newPubSocket(new Bind("tcp://127.0.0.1:1237"));
 		//pubSocket.tell("test");
 	}
