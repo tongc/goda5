73cd07fb9c45e46f3be8d9fff45f1fd7e70eaec1
chentongtc@gmail.com <chentongtc@gmail.com>
2014-07-02 23:16:58 +0100

    angularjs playaround

--- hagendaz/web/src/main/webapp/scripts/controller.js
+++ hagendaz/web/src/main/webapp/scripts/controller.js
@@ -1,6 +1,6 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $q) {
+phonecatApp.controller('PhoneListCtrl', function ($scope, $q, $http) {
   $scope.phones = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.'},
@@ -10,6 +10,23 @@ phonecatApp.controller('PhoneListCtrl', function ($scope, $q) {
      'snippet': 'The Next, Next Generation tablet.'}
   ];
   
+  function x(y) {
+	  y();
+  }
+  
+  function a() {
+	  var deferred = $q.defer();
+	  x(function() {
+		  deferred.resolve("aaabbbccc");
+	  });
+	  return deferred.promise;
+  }
+  
+  a().then(function(data) {
+	  alert(data);
+  });
+ 
+  alert("ddddd");
   function asyncGreet(name) {
 	    var deferred = $q.defer();
 
@@ -36,4 +53,24 @@ phonecatApp.controller('PhoneListCtrl', function ($scope, $q) {
 	    alert('Got notification: ' + update);
 	  });
 	  alert("passed");
+	  
+	  var fun = function() {
+		  var deferred = $q.defer();
+		  $http({method: 'GET', url: '/goda5/home'}).
+	    success(function(data, status, headers, config) {
+	      deferred.resolve(data);
+	    }).
+	    error(function(data, status, headers, config) {
+	    	deferred.reject(data);
+	    });
+	  	return deferred.promise;
+	  };
+	  
+	  fun().then(function(greeting) {
+	    alert('Success: ' + greeting);
+	  }, function(reason) {
+	    alert('Failed: ' + reason);
+	  }, function(update) {
+	    alert('Got notification: ' + update);
+	  });
 });
