bf128b05eba54516ae04c7ba988b70aa00a85900
chentongtc@gmail.com <chentongtc@gmail.com>
2014-07-13 22:18:19 +0100

    jmockit for guava tools

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/GuavaFutureAndAsyncTest.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/GuavaFutureAndAsyncTest.java
@@ -1,80 +1,57 @@
 package com.goda5.hagendaz.common;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.junit.Test;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import mockit.integration.junit4.JMockit;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.goda5.hagendaz.common.GuavaFutureAndAsync.MyCallable;
+import com.goda5.hagendaz.common.GuavaFutureAndAsync.MyCallableHandler;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
+@RunWith(JMockit.class)
 public class GuavaFutureAndAsyncTest {
+	@Tested
+	private GuavaFutureAndAsync c = new GuavaFutureAndAsync();
+	
+	@Mocked 
+	private Executors e;
+	
+	@Mocked
+	private ExecutorService es;
+	
+	@Mocked
+	private ListeningExecutorService les;
+	
+	@Mocked
+	private ListenableFuture<Boolean> f;
+	
+	@Mocked
+	private MyCallable callable;
+	
+	@Mocked
+	private MyCallableHandler handler;
+	
 	@Test
-	public void a() throws InterruptedException {
-		ListeningExecutorService l = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(5));
-		ListenableFuture<Boolean> f = l.submit(new MyCallable("1"));
-		f.addListener(new MyCallableHandler(f, "1"), MoreExecutors.sameThreadExecutor());
-		
-		ListenableFuture<Boolean> f1 = l.submit(new MyCallable("2"));
-		f1.addListener(new MyCallableHandler(f1, "2"), MoreExecutors.sameThreadExecutor());
-		
-		ListenableFuture<Boolean> f2 = l.submit(new MyCallable("3"));
-		f2.addListener(new MyCallableHandler(f2, "3"), MoreExecutors.sameThreadExecutor());
-		
-		ListenableFuture<Boolean> f3 = l.submit(new MyCallable("4"));
-		Futures.addCallback(f3, new FutureCallback<Boolean>() {
-			@Override
-			public void onSuccess(Boolean result) {
-				System.out.println(Thread.currentThread().getName());
-				System.out.println(result);
-			}
-			@Override
-			public void onFailure(Throwable t) {
-				System.out.println(t.getMessage());
-			}
-		});
-		
-		System.out.println(Thread.currentThread().getName());
-		Thread.sleep(3000);
-	}
-	
-	class MyCallableHandler implements Runnable {
-		private final ListenableFuture<Boolean> f;
-		private String name;
-
-		public MyCallableHandler(ListenableFuture<Boolean> f, String name) {
-			this.f = f;
-			this.name = name;
-		}
-		
-		@Override
-		public void run() {
-			try {
-				System.out.println(name + " : " + Thread.currentThread().getName());
-				System.out.println(f.get());
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	class MyCallable implements Callable<Boolean> {
-		private String name;
-		public MyCallable(String name) {
-			this.name = name;
-		}
-		
-		@Override
-		public Boolean call() throws Exception {
-			Thread.sleep(1000);
-			System.out.println(name + " called " + Thread.currentThread().getName());
-			return Boolean.TRUE;
-		}
+	public void t() throws InterruptedException {
+		new NonStrictExpectations() {{
+			Executors.newFixedThreadPool(5); result = es;
+			MoreExecutors.listeningDecorator(es); result = les;
+			MoreExecutors.sameThreadExecutor(); result = les;
+			c.new MyCallable((String)any); result = callable; times = 4;
+			les.submit((MyCallable)any); result = f;
+			c.new MyCallableHandler(f, (String)any); result = handler; times = 4;
+			f.addListener((MyCallableHandler)any, les); times = 5;
+		}};
+		c.execute(); 
 	}
 }
