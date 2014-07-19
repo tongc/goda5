9174a629481763ba4d730834bc77dbef35db9711
Tong <chentongtc@gmail.com>
2014-07-19 22:10:02 +0100

    updated nio watching service

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/NioWatchService.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/NioWatchService.java
@@ -2,8 +2,6 @@ package com.goda5.hagendaz.common;
 
 import java.io.IOException;
 import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
@@ -14,52 +12,25 @@ import java.nio.file.WatchEvent.Kind;
 
 public class NioWatchService {
 	public static void watchDirectoryPath(Path path) {
-		// Sanity check - Check if path is a folder
-		try {
-			Boolean isFolder = (Boolean) Files.getAttribute(path,
-					"basic:isDirectory", LinkOption.NOFOLLOW_LINKS);
-			if (!isFolder) {
-				throw new IllegalArgumentException("Path: " + path + " is not a folder");
-			}
-		} catch (IOException ioe) {
-			// Folder does not exists
-			ioe.printStackTrace();
-		}
-		
 		System.out.println("Watching path: " + path);
-		
-		// We obtain the file system of the Path
 		FileSystem fs = path.getFileSystem ();
-		
-		// We create the new WatchService using the new try() block
 		try(WatchService service = fs.newWatchService()) {
-			
-			// We register the path to the service
-			// We watch for creation events
 			path.register(service, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
-			
-			// Start the infinite polling loop
 			WatchKey key = null;
 			while(true) {
 				key = service.take();
-				
-				// Dequeueing events
 				Kind<?> kind = null;
 				for(WatchEvent<?> watchEvent : key.pollEvents()) {
-					// Get the type of the event
 					kind = watchEvent.kind();
 					if (StandardWatchEventKinds.OVERFLOW == kind) {
-						continue; //loop
+						continue;
 					} else {
-						// A new Path was created 
 						Path newPath = ((WatchEvent<Path>) watchEvent).context();
-						// Output
-						System.out.println("operation on " + newPath);
+						System.out.println("operation kind " + kind + " path " + newPath);
 					}
 				}
-				
 				if(!key.reset()) {
-					break; //loop
+					break;
 				}
 			}
 			
@@ -68,11 +39,10 @@ public class NioWatchService {
 		} catch(InterruptedException ie) {
 			ie.printStackTrace();
 		}
-		
 	}
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-    	Path monitorPath = Paths.get("C:\\Users\\v626284\\Downloads");
+    	Path monitorPath = Paths.get("/home/tong/Downloads");
     	watchDirectoryPath(monitorPath);
 //    	try (final WatchService watchService = FileSystems.getDefault().newWatchService()) {
 //	        monitorPath.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
