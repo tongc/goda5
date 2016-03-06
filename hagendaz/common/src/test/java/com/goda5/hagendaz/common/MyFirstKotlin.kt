07172e0d8550d26bc990659c86deda222e3a48e9
chentongtc@gmail.com <chentongtc@gmail.com>
2016-03-06 17:57:26 +0000

    test

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/MyFirstKotlin.kt
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/MyFirstKotlin.kt
@@ -4,6 +4,6 @@ package com.goda5.hagendaz.common
  * Created by tong on 18/12/2015.
  */
 fun main(args: Array<String>) {
-    val numbers = listOf(1, 2, 3);
-    println("is this same as java?")
+//    val numbers = listOf(1, 2, 3);
+//    println("is this same as java?")
 }
