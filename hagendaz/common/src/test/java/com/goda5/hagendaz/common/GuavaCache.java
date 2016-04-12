f52a755f7135927c519e5183dacd2f9089a91419
chentongtc@gmail.com <chentongtc@gmail.com>
2016-04-12 20:26:19 +0100

    play with guava cache.

---
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/GuavaCache.java
@@ -0,0 +1,52 @@
+package com.goda5.hagendaz.common;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by tong on 12/04/2016.
+ */
+public class GuavaCache {
+    @Test
+    public void testNormalCache() {
+
+    }
+
+    @Test
+    public void testGuavaCache() throws ExecutionException {
+        LoadingCache<Integer, Object> graphs = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .build(
+                        new CacheLoader<Integer, Object>() {
+                            public Object load(Integer key) {
+                                return expensiveOperation(key);
+                            }
+                        });
+
+        benchmark(o -> graphs.getUnchecked((Integer)o), 1);
+        benchmark(o -> graphs.getUnchecked((Integer)o), 1);
+        benchmark(o -> graphs.getUnchecked((Integer)o), 1);
+    }
+
+    private void benchmark(java.util.function.Consumer<Object> consumer, Object parameter) {
+        long start = System.currentTimeMillis();
+        consumer.accept(parameter);
+        long end = System.currentTimeMillis();
+        System.out.printf("time taken: %s\n", end - start);
+    }
+
+    private Object expensiveOperation(Integer key) {
+        //nothing is more expensive than sleep.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return ThreadLocalRandom.current().nextInt(100);
+    }
+}
