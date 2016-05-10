6eab5f7b16433b622a06a4d4ce8a7b0963e779e7
chentongtc@gmail.com <chentongtc@gmail.com>
2016-05-10 21:19:31 +0100

    thread jmx

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/DeadLockDetectionJmx.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/DeadLockDetectionJmx.java
@@ -1,13 +1,41 @@
 package com.goda5.hagendaz.common;
 
 import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by tong on 10/05/2016.
  */
 public class DeadLockDetectionJmx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock l1 = new ReentrantLock();
+        ReentrantLock l2 = new ReentrantLock();
+        new Thread(() -> {
+            l1.lock();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            l2.lock();
+        }).start();
+
+        new Thread(() -> {
+            l2.lock();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            l1.lock();
+        }).start();
+
+        Thread.sleep(3000);
         System.out.printf("-----------------\n");
+        for(long id:ManagementFactory.getThreadMXBean().findDeadlockedThreads()) {
+            System.out.printf("deadlock thread: %s\n", ManagementFactory.getThreadMXBean().getThreadInfo(id));
+        }
         System.out.printf("%s", ManagementFactory.getThreadMXBean().getThreadCount());
         for(long id:ManagementFactory.getThreadMXBean().getAllThreadIds()) {
             System.out.printf("%s\n", ManagementFactory.getThreadMXBean().getThreadInfo(id));
