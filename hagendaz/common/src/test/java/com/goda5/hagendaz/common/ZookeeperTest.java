4ec49d798a9372dad2696074b7a7566d83d254c1
chentongtc@gmail.com <chentongtc@gmail.com>
2016-05-15 20:24:15 +0100

    attempt zookeeper client connection to the docker inside vagrant.

---
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/ZookeeperTest.java
@@ -0,0 +1,24 @@
+package com.goda5.hagendaz.common;
+
+import com.google.common.collect.Lists;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
+
+/**
+ * Created by tong on 15/05/2016.
+ */
+public class ZookeeperTest {
+    public static void main(String[] args) {
+        try {
+            ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:12181", 3000, null);
+            System.out.println(zooKeeper.getState());
+            System.out.println(zooKeeper.getSessionId());
+            System.out.println(zooKeeper.getSessionTimeout());
+            zooKeeper.create("/abc", "".getBytes(), Lists.newArrayList(new ACL(1, new Id("a", "b"))), CreateMode.PERSISTENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
