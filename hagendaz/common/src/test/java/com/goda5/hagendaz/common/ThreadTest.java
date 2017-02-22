6631a3b879dab6c0093e3cccf640fb107dfb90a9
chentongtc@gmail.com <chentongtc@gmail.com>
2017-02-22 21:26:38 +0000

    basic thread tests.

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/ThreadTest.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/ThreadTest.java
@@ -2,9 +2,15 @@ package com.goda5.hagendaz.common;
 
 import org.junit.Test;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MonitorInfo;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 /**
@@ -131,5 +137,79 @@ public class ThreadTest {
         Thread.sleep(100);
         System.out.println(a.getState());
         Thread.sleep(1000);
+    }
+
+    @Test
+    public void deadlock() throws InterruptedException {
+        Lock c = new ReentrantLock();
+        c.lock();
+        System.out.println(Thread.holdsLock(c));
+
+        synchronized (c) {
+            System.out.println(Thread.holdsLock(c));
+        }
+
+        Lock a = new ReentrantLock();
+        Lock b = new ReentrantLock();
+        System.out.println(a);
+        System.out.println(b);
+
+        Thread aa = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    a.lock();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("holds a " + Thread.holdsLock(a));
+                    System.out.println("holds b " + Thread.holdsLock(b));
+                    b.lock();
+                    a.unlock();
+                    b.unlock();
+                }
+            }
+        });
+
+        Thread bb = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    b.lock();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("holds a " + Thread.holdsLock(a));
+                    System.out.println("holds b " + Thread.holdsLock(b));
+                    a.lock();
+                    b.unlock();
+                    a.unlock();
+                }
+            }
+        });
+
+        aa.start();
+        bb.start();
+
+        Thread.sleep(1000);
+        System.out.println("state " + aa.getState());
+        System.out.println("state " + bb.getState());
+
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] ti = bean.getThreadInfo(bean.getAllThreadIds(), true, true);
+        for (ThreadInfo threadInfo:ti) {
+            System.out.println("lock name " + threadInfo.getLockName());
+            for(MonitorInfo monitorInfo:threadInfo.getLockedMonitors()) {
+                System.out.println("line number " +  monitorInfo.getLockedStackFrame().getLineNumber());
+            }
+        }
+
+        Thread.sleep(5000);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
