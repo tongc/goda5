fd940d440294d1ba753e07738366de669fb2e945
chentongtc@gmail.com <chentongtc@gmail.com>
2017-03-12 10:21:27 +0000

    play with ieee754

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/ThreadTest.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/ThreadTest.java
@@ -373,5 +373,20 @@ public class ThreadTest {
         System.out.println(0.22599999999999998D * 5);
 
         System.out.println(new BigDecimal(d, MathContext.DECIMAL32));
+//bit 31   30 - 23   22 - 0
+//     0   01111111  00000000000000000000000 //positive number so the sign bit is not specified
+        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(1F)));
+//bit 31   30 - 23   22 - 0
+//     1   01111111  00000000000000000000000 //negative number so the sign bit is set to 10
+        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(-1F)));
+//bit 31   30 - 23   22 - 0
+//     0   10000000  00000000000000000000000
+        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(2F)));
+//bit 31   30 - 23   22 - 0
+//     1   10000000  00000000000000000000000
+        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(-2F)));
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(Float.floatToRawIntBits(0F)));
+        System.out.println(Long.toBinaryString(Double.doubleToRawLongBits(1D)));
     }
 }
