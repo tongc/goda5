77d96b141af371557f38b8dd3d10ccaaba559c6b
tong <chentongtc@gmail.com>
2020-09-09 21:21:26 +0100

    base systems

--- hagendaz/common/src/test/java/com/goda5/hagendaz/common/BaseSystems.java
+++ hagendaz/common/src/test/java/com/goda5/hagendaz/common/BaseSystems.java
@@ -4,6 +4,18 @@ public class BaseSystems {
     public static void main(String[] args) {
         System.out.println(new BaseSystems().base3());
         System.out.println(new BaseSystems().base16());
+        System.out.println(new BaseSystems().base64());
+    }
+
+    public String base64() {
+        char[] baseSystem = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'};
+        int input = 262143;
+        String result = "";
+        while(input > 0) {
+            result += baseSystem[input % 64];
+            input /= 64;
+        }
+        return result;
     }
 
     public String base16() {
